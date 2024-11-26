7daaa04963752b39828e70dfdbfc7f5ad13d995c
Nitesh Kumar <niteshkumaro7@hotmail.com>
2024-11-26 21:20:22 +0530

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
@@ -15,9 +16,11 @@ const Navbar = () => {
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
-            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
-            <span className="text-xl tracking-tight">Fresher</span>
-          </div>
+  <Link to="/" className="flex items-center">
+    <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
+    <span className="text-xl tracking-tight">Fresher</span>
+  </Link>
+</div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
               <li key={index}>
