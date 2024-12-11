0ad713b54054f51a24c9039b93dd6fed60360624
Nitesh Kumar <niteshkumaro7@hotmail.com>
2024-12-11 17:51:58 +0530

    Update FeatureSection.jsx

--- frontend/src/components/FeatureSection.jsx
+++ frontend/src/components/FeatureSection.jsx
@@ -36,7 +36,7 @@ const FeatureSection = () => {
         <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
           Fresher's and Farewell Party{" "}
           <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-            2024
+            2025
           </span>
         </h2>
       </div>
